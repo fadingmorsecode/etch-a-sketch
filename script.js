51271bcd3bdf25bb8782b0201f55b72de23e3240
Drake Williamson <williamson.drake@outlook.com>
2022-10-01 11:19:14 -0700

    Fix specificity of new squareDiv styles

--- script.js
+++ script.js
@@ -4,17 +4,45 @@ const gridContainer = document.querySelector('.grid-container');
 
 for (let i = 0; i < 256; i++) {
     const squareDiv = document.createElement('div');
-    squareDiv.classList.add('square-style')
+    squareDiv.classList.add('square-style');
     gridContainer.appendChild(squareDiv);
+    squareDiv.addEventListener('mouseover', () => {
+        squareDiv.classList.add('square-hover');
+    });
 }
 
 
 // Create Hover Effect Style
 
-const hoverEffect = document.querySelectorAll('.square-style');
 
-for (let i = 0; i < hoverEffect.length; i++) {
-    hoverEffect[i].addEventListener('mouseover', () => {
-        hoverEffect[i].classList.add('square-hover');
+// New Grid
+
+const newGridBttn = document.querySelector('.bttn');
+
+newGridBttn.addEventListener('click', () => {
+    let newGrid = prompt("Please enter how many squares you would like per side (Max 100)");
+   const previousGrid = document.querySelectorAll('.square-style');
+   for (let i = 0; i < previousGrid.length; i++) {
+    gridContainer.removeChild(previousGrid[i]);
+   } 
+
+   let sizecalculation = (600 / newGrid) -1;
+
+   console.log(sizecalculation);
+
+    for (let i = 0; i < (newGrid * newGrid); i++) {
+    const squareDiv = document.createElement('div');
+    squareDiv.classList.add('new-square-style')
+    squareDiv.style.height = `${sizecalculation}px`;
+    squareDiv.style.width = `${sizecalculation}px`;
+    gridContainer.appendChild(squareDiv);
+    squareDiv.addEventListener('mouseover', () => {
+        squareDiv.classList.add('square-hover');
     });
-}
+   }
+
+});
+
+
+
+
