dcf3e6f12d3e097b44bc8dca49ec8aa88b0ca011
Drake Williamson <williamson.drake@outlook.com>
2022-09-30 21:33:45 -0700

    Redo hover effect. Make effect permanent.

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+// Create 256 Divs 
+
 const gridContainer = document.querySelector('.grid-container');
 
 for (let i = 0; i < 256; i++) {
     const squareDiv = document.createElement('div');
     squareDiv.classList.add('square-style')
     gridContainer.appendChild(squareDiv);
-    console.log(i);
 }
 
+
+// Create Hover Effect Style
+
+const hoverEffect = document.querySelectorAll('.square-style');
+
+for (let i = 0; i < hoverEffect.length; i++) {
+    hoverEffect[i].addEventListener('mouseover', () => {
+        hoverEffect[i].classList.add('square-hover');
+        console.log('yes?')
+    });
+}
